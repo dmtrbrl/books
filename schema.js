7952d63f2dd96687c42ca5c61ad369aec17f0b86
Dmytro Barylo <dmytrobarylo@Dmytros-MacBook-Pro.local>
2018-03-11 00:02:33 +0200

    api initial commit

---
+++ schema.js
@@ -0,0 +1,73 @@
+const fetch = require('node-fetch');
+const util = require('util');
+const parseXML = util.promisify(require('xml2js').parseString);
+
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLList
+} = require('graphql');
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'Authors book',
+    fields: () => ({
+        title: {
+            type: GraphQLString,
+            resolve: xml => xml.title[0]
+        },
+        isbn: {
+            type: GraphQLString,
+            resolve: xml => xml.isbn[0]
+        },
+        cover: {
+            type: GraphQLString,
+            resolve: xml => xml.image_url[0]
+        }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'Author info',
+    fields: () => ({
+        name: {
+            type: GraphQLString,
+            resolve: xml => xml.GoodreadsResponse.author[0].name[0]
+        },
+        about: {
+            type: GraphQLString,
+            resolve: xml => xml.GoodreadsResponse.author[0].about[0]
+        },
+        photo: {
+            type: GraphQLString,
+            resolve: xml => xml.GoodreadsResponse.author[0].image_url[0]
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: xml => xml.GoodreadsResponse.author[0].books[0].book
+        }
+    })
+});
+
+module.exports = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Query',
+        description: 'Main query',
+        fields: () => ({
+            author: {
+                type: AuthorType,
+                args: {
+                    id: { type: GraphQLInt }
+                },
+                resolve: (root, args) => fetch(
+                    `https://www.goodreads.com/author/show/${args.id}?format=xml&key=W4rcG3xsfWe4LPpD65RyKA`
+                )
+                .then(resp => resp.text())
+                .then(parseXML)
+            }
+        })
+    })
+});
