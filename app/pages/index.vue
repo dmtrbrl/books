30f6066e58549759ea4a23cf2c24d8b14e922614
Dmytro Barylo <bdpochta@gmail.com>
2018-03-14 11:27:09 +0200

    Add nuxt app

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        books-app
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
