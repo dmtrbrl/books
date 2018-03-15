52859af7eaa9497bd00b1ca42646d380a364869f
Dmytro Barylo <bdpochta@gmail.com>
2018-03-16 01:12:22 +0200

    Add header

---
+++ app/components/header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header class="header">
+        <div class="container header__container">
+            <nuxt-link to="/" class="logo">
+                <img class="logo__img" src="../static/logo.svg" alt="Books">
+            </nuxt-link>
+        </div>
+    </header>
+</template>
+
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css?family=Quicksand:700');
+    .header{
+        width: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        background: #2B2B2B;
+        z-index: 100;
+        &__container{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            height: 60px;
+        }
+    }
+    .logo{
+        display: block;
+        width: 80px;
+        &__img{
+            display: block;
+            width: 100%;
+            height: auto;
+        }
+    }
+</style>
+
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,47 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+    <div class="main">
+        <Header></Header>
+        <nuxt/>
+    </div>
 </template>
+
+<script>
+import Header from '../components/header'
+export default {
+    components: {
+        Header
+    }
+}
+</script>
+
+
 
 <style lang="scss">
     @import url('https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css');
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,700&subset=cyrillic,cyrillic-ext,latin-ext,vietnamese');
+
+    // Box-sizing
+    *{
+        box-sizing: border-box;
+    }
+
+    body{
+        padding-top: 50px;
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    // Transitions
+    .page-enter-active, .page-leave-active {
+        transition: opacity .5s
+    }
+    .page-enter, .page-leave-active {
+        opacity: 0
+    }
+
+    // Container
+    .container{
+        max-width: 1280px;
+        padding: 0 30px;
+        margin: 0 auto;
+    }
 </style>
